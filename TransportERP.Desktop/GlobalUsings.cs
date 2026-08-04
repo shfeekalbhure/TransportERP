04d030fa96832968738667fa84209cf2897fa1b1
shfeekalbhure <shfeek01@gmail.com>
2026-08-04 05:51:25 +0300

    إصلاح مرجع RequiredTextBox في شاشة الدول

---
+++ TransportERP.Desktop/GlobalUsings.cs
@@ -0,0 +1,3 @@
+// الاستيرادات العامة المشتركة لمشروع TransportERP.Desktop.
+// يتيح استخدام عناصر الإدخال الموحدة داخل ملفات النماذج والمصمم دون تكرار using.
+global using TransportERP.Desktop.Controls;
