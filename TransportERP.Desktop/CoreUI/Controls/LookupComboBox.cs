ec48b6580593029584c14a87056707c9d11df4b6
shfeekalbhure <shfeek01@gmail.com>
2026-08-04 04:28:43 +0300

    إلغاء الرسم اليدوي المسبب لاستثناء Win32 في القوائم المنسدلة

--- TransportERP.Desktop/CoreUI/Controls/LookupComboBox.cs
+++ TransportERP.Desktop/CoreUI/Controls/LookupComboBox.cs
@@ -22,7 +22,6 @@ public sealed class LookupComboBox : ComboBox
         Enter += HandleEnter;
         Leave += HandleLeave;
         SelectedIndexChanged += (_, _) => UpdateVisualState();
-        DrawItem += DrawArabicItem;
     }
 
     [Category("TransportERP")]
@@ -64,13 +63,14 @@ public sealed class LookupComboBox : ComboBox
                 Items.Add(item);
             }
 
-            SelectedIndex = selectFirstItem && Items.Count > 0 ? 0 : -1;
+            SelectedIndex = selectFirstItem && Items.Count > 0
+                ? 0
+                : -1;
         }
         finally
         {
             EndUpdate();
             UpdateVisualState();
-            Invalidate();
         }
     }
 
@@ -103,18 +103,13 @@ public sealed class LookupComboBox : ComboBox
         UpdateVisualState();
     }
 
-    protected override void OnHandleCreated(EventArgs e)
-    {
-        base.OnHandleCreated(e);
-        RightToLeft = RightToLeft.Yes;
-        Invalidate();
-    }
-
+    /// <summary>
+    /// تطبيق تنسيق آمن ومتوافق مع مصمم WinForms دون رسم يدوي.
+    /// </summary>
     private void ApplyDefaultStyle()
     {
         DropDownStyle = ComboBoxStyle.DropDownList;
-        DrawMode = DrawMode.OwnerDrawFixed;
-        ItemHeight = 28;
+        DrawMode = DrawMode.Normal;
         FlatStyle = FlatStyle.Flat;
         Font = UiTheme.CreateRegularFont(10.5F);
         ForeColor = UiTheme.HeadingText;
@@ -122,46 +117,15 @@ public sealed class LookupComboBox : ComboBox
         UpdateVisualState();
     }
 
-    /// <summary>
-    /// رسم العنصر المختار وعناصر القائمة بمحاذاة عربية إلى أقصى اليمين.
-    /// </summary>
-    private void DrawArabicItem(object? sender, DrawItemEventArgs e)
-    {
-        e.DrawBackground();
-
-        if (e.Index < 0 || e.Index >= Items.Count)
-        {
-            return;
-        }
-
-        var text = GetItemText(Items[e.Index]);
-        var textColor = (e.State & DrawItemState.Selected) == DrawItemState.Selected
-            ? SystemColors.HighlightText
-            : ForeColor;
-        var textBounds = Rectangle.Inflate(e.Bounds, -8, 0);
-
-        TextRenderer.DrawText(
-            e.Graphics,
-            text,
-            Font,
-            textBounds,
-            textColor,
-            TextFormatFlags.Right |
-            TextFormatFlags.VerticalCenter |
-            TextFormatFlags.RightToLeft |
-            TextFormatFlags.EndEllipsis |
-            TextFormatFlags.NoPrefix);
-
-        e.DrawFocusRectangle();
-    }
-
     private void HandleEnter(object? sender, EventArgs e)
     {
+        RightToLeft = RightToLeft.Yes;
         BackColor = UiTheme.FocusedInputBackground;
     }
 
     private void HandleLeave(object? sender, EventArgs e)
     {
+        RightToLeft = RightToLeft.Yes;
         UpdateVisualState();
     }
 
