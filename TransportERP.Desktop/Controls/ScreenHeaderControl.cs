9d096928cb2a9ac7915f3a62f66168b5432399a2
shfeekalbhure <shfeek01@gmail.com>
2026-08-04 18:00:33 +0300

    تفعيل أحداث الرأس المشترك لشاشات النظام

--- TransportERP.Desktop/Controls/ScreenHeaderControl.cs
+++ TransportERP.Desktop/Controls/ScreenHeaderControl.cs
@@ -9,6 +9,18 @@ public sealed class ScreenHeaderControl : UserControl
     private readonly Label _titleLabel;
     private readonly Label _breadcrumbLabel;
     private readonly Label _recordLabel;
+    private readonly Button _btnNew;
+    private readonly Button _btnSave;
+    private readonly Button _btnEdit;
+    private readonly Button _btnSearch;
+    private readonly Button _btnFirst;
+    private readonly Button _btnPrevious;
+    private readonly Button _btnNext;
+    private readonly Button _btnLast;
+    private readonly Button _btnDelete;
+    private readonly Button _btnPrint;
+    private readonly Button _btnRefresh;
+    private readonly Button _btnClose;
 
     public ScreenHeaderControl()
     {
@@ -77,12 +89,12 @@ public sealed class ScreenHeaderControl : UserControl
             RightToLeft = RightToLeft.Yes
         };
 
-        var btnNew = CreateActionButton("جديد", Color.FromArgb(28, 105, 225), Color.White);
-        var btnSave = CreateActionButton("حفظ", Color.FromArgb(45, 157, 83), Color.White);
-        var btnEdit = CreateActionButton("تعديل", Color.FromArgb(245, 145, 20), Color.White);
-        var btnSearch = CreateActionButton("بحث", Color.FromArgb(28, 105, 225), Color.White);
-        var btnFirst = CreateNavigationButton("|◀", "الأول");
-        var btnPrevious = CreateNavigationButton("◀", "السابق");
+        _btnNew = CreateActionButton("جديد", Color.FromArgb(28, 105, 225), Color.White);
+        _btnSave = CreateActionButton("حفظ", Color.FromArgb(45, 157, 83), Color.White);
+        _btnEdit = CreateActionButton("تعديل", Color.FromArgb(245, 145, 20), Color.White);
+        _btnSearch = CreateActionButton("بحث", Color.FromArgb(28, 105, 225), Color.White);
+        _btnFirst = CreateNavigationButton("|◀", "الأول");
+        _btnPrevious = CreateNavigationButton("◀", "السابق");
 
         _recordLabel = new Label
         {
@@ -97,24 +109,50 @@ public sealed class ScreenHeaderControl : UserControl
             Margin = new Padding(4, 0, 4, 0)
         };
 
-        var btnNext = CreateNavigationButton("▶", "التالي");
-        var btnLast = CreateNavigationButton("▶|", "الأخير");
-        var btnDelete = CreateActionButton("حذف", Color.FromArgb(225, 55, 55), Color.White);
-        var btnPrint = CreateActionButton("طباعة", Color.White, Color.FromArgb(33, 45, 65));
-        var btnRefresh = CreateActionButton("تحديث", Color.White, Color.FromArgb(33, 45, 65));
-        var btnClose = CreateActionButton("إغلاق", Color.White, Color.FromArgb(33, 45, 65));
+        _btnNext = CreateNavigationButton("▶", "التالي");
+        _btnLast = CreateNavigationButton("▶|", "الأخير");
+        _btnDelete = CreateActionButton("حذف", Color.FromArgb(225, 55, 55), Color.White);
+        _btnPrint = CreateActionButton("طباعة", Color.White, Color.FromArgb(33, 45, 65));
+        _btnRefresh = CreateActionButton("تحديث", Color.White, Color.FromArgb(33, 45, 65));
+        _btnClose = CreateActionButton("إغلاق", Color.White, Color.FromArgb(33, 45, 65));
+
+        _btnNew.Click += (_, e) => NewClicked?.Invoke(this, e);
+        _btnSave.Click += (_, e) => SaveClicked?.Invoke(this, e);
+        _btnEdit.Click += (_, e) => EditClicked?.Invoke(this, e);
+        _btnSearch.Click += (_, e) => SearchClicked?.Invoke(this, e);
+        _btnFirst.Click += (_, e) => FirstClicked?.Invoke(this, e);
+        _btnPrevious.Click += (_, e) => PreviousClicked?.Invoke(this, e);
+        _btnNext.Click += (_, e) => NextClicked?.Invoke(this, e);
+        _btnLast.Click += (_, e) => LastClicked?.Invoke(this, e);
+        _btnDelete.Click += (_, e) => DeleteClicked?.Invoke(this, e);
+        _btnPrint.Click += (_, e) => PrintClicked?.Invoke(this, e);
+        _btnRefresh.Click += (_, e) => RefreshClicked?.Invoke(this, e);
+        _btnClose.Click += (_, e) => CloseClicked?.Invoke(this, e);
 
         toolbar.Controls.AddRange(new Control[]
         {
-            btnNew, btnSave, btnEdit, btnSearch,
-            btnFirst, btnPrevious, _recordLabel, btnNext, btnLast,
-            btnDelete, btnPrint, btnRefresh, btnClose
+            _btnNew, _btnSave, _btnEdit, _btnSearch,
+            _btnFirst, _btnPrevious, _recordLabel, _btnNext, _btnLast,
+            _btnDelete, _btnPrint, _btnRefresh, _btnClose
         });
 
         root.Controls.Add(titleRow, 0, 0);
         root.Controls.Add(toolbar, 0, 1);
         Controls.Add(root);
     }
+
+    public event EventHandler? NewClicked;
+    public event EventHandler? SaveClicked;
+    public event EventHandler? EditClicked;
+    public event EventHandler? SearchClicked;
+    public event EventHandler? FirstClicked;
+    public event EventHandler? PreviousClicked;
+    public event EventHandler? NextClicked;
+    public event EventHandler? LastClicked;
+    public event EventHandler? DeleteClicked;
+    public event EventHandler? PrintClicked;
+    public event EventHandler? RefreshClicked;
+    public event EventHandler? CloseClicked;
 
     public string ScreenTitle
     {
@@ -132,6 +170,14 @@ public sealed class ScreenHeaderControl : UserControl
     {
         get => _recordLabel.Text;
         set => _recordLabel.Text = value;
+    }
+
+    public void SetNavigationState(bool hasRecords, bool isFirst, bool isLast)
+    {
+        _btnFirst.Enabled = hasRecords && !isFirst;
+        _btnPrevious.Enabled = hasRecords && !isFirst;
+        _btnNext.Enabled = hasRecords && !isLast;
+        _btnLast.Enabled = hasRecords && !isLast;
     }
 
     private static Button CreateActionButton(string text, Color backColor, Color foreColor)
