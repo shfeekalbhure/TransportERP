521684968570a62b71ff351c79caea321fc1cec2
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 00:23:25 +0300

    Implement GEN-009 العملات WinForms designer layout

---
+++ TransportERP.Desktop/Forms/Setup/Geographic/FrmCurrencies.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop.Forms.Setup.General;
+
+public partial class FrmCurrencies : Form
+{
+    public FrmCurrencies()
+    {
+        InitializeComponent();
+    }
+}
