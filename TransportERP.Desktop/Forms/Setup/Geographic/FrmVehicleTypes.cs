a9f71bc6af1141607c3b278b06fe8ba8dff1e392
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 00:23:22 +0300

    Implement GEN-008 أنواع المركبات WinForms designer layout

---
+++ TransportERP.Desktop/Forms/Setup/Geographic/FrmVehicleTypes.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop.Forms.Setup.General;
+
+public partial class FrmVehicleTypes : Form
+{
+    public FrmVehicleTypes()
+    {
+        InitializeComponent();
+    }
+}
