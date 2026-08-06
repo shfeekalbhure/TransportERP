aac101c27d03db6063c51583f2ed2d3f7541aa3d
shfeekalbhure <shfeek01@gmail.com>
2026-08-06 07:46:59 +0300

    Expose UnsavedChangesChoice as global alias

--- TransportERP.Desktop/GlobalTypeAliases.cs
+++ TransportERP.Desktop/GlobalTypeAliases.cs
@@ -1,3 +1,6 @@
 // الأسماء المستعارة العامة للأنواع التي تتكرر أسماؤها بين حزم النظام.
 // شاشة السنوات المالية المستخدمة مباشرة في قائمة التهيئة العامة هي GEN-013.
 global using FrmFiscalYears = TransportERP.Desktop.Forms.Setup.General.FrmFiscalYears;
+
+// قرار التعامل مع التعديلات غير المحفوظة متاح لجميع الشاشات دون تكرار using.
+global using UnsavedChangesChoice = TransportERP.Desktop.Models.UnsavedChangesChoice;
