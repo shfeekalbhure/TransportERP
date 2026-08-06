1e52fdb2bac3d0c44a8859605c1dfeeb1a30816c
shfeekalbhure <shfeek01@gmail.com>
2026-08-06 07:36:26 +0300

    Resolve ambiguous FrmFiscalYears reference with explicit global alias

---
+++ TransportERP.Desktop/GlobalTypeAliases.cs
@@ -0,0 +1,3 @@
+// الأسماء المستعارة العامة للأنواع التي تتكرر أسماؤها بين حزم النظام.
+// شاشة السنوات المالية المستخدمة مباشرة في قائمة التهيئة العامة هي GEN-013.
+global using FrmFiscalYears = TransportERP.Desktop.Forms.Setup.General.FrmFiscalYears;
