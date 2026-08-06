6bd803dd02571778f0539950f1cbab5efa06cafa
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 02:23:04 +0300

    إضافة واجهة المرحلة الثانية: FrmGen008VehicleTypes

---
+++ TransportERP.Desktop/Forms/Setup/General/FrmGen008VehicleTypes.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop;
+
+/// <summary>واجهة GEN-008 — أنواع المركبات وفق مراجع الإصدار 1.13.</summary>
+public partial class FrmGen008VehicleTypes : Form
+{
+    public FrmGen008VehicleTypes()
+    {
+        InitializeComponent();
+    }
+}
