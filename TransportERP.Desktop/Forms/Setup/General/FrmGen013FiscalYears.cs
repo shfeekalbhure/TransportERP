82e7eabd592df9d5ee2ec6e4203e3e54f2a6803e
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 02:23:21 +0300

    إضافة واجهة المرحلة الثانية: FrmGen013FiscalYears

---
+++ TransportERP.Desktop/Forms/Setup/General/FrmGen013FiscalYears.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop;
+
+/// <summary>واجهة GEN-013 — السنوات المالية وفق مراجع الإصدار 1.13.</summary>
+public partial class FrmGen013FiscalYears : Form
+{
+    public FrmGen013FiscalYears()
+    {
+        InitializeComponent();
+    }
+}
