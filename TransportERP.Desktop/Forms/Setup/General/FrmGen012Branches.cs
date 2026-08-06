a9a2f9ce5680000aa73a48abdce8ae4868714109
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 02:23:14 +0300

    إضافة واجهة المرحلة الثانية: FrmGen012Branches

---
+++ TransportERP.Desktop/Forms/Setup/General/FrmGen012Branches.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop;
+
+/// <summary>واجهة GEN-012 — الفروع وفق مراجع الإصدار 1.13.</summary>
+public partial class FrmGen012Branches : Form
+{
+    public FrmGen012Branches()
+    {
+        InitializeComponent();
+    }
+}
