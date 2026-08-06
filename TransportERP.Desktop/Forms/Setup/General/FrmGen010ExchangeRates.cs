646ebf0174cede128c394457cf4914f65973ddc4
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 02:23:09 +0300

    إضافة واجهة المرحلة الثانية: FrmGen010ExchangeRates

---
+++ TransportERP.Desktop/Forms/Setup/General/FrmGen010ExchangeRates.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop;
+
+/// <summary>واجهة GEN-010 — أسعار الصرف وفق مراجع الإصدار 1.13.</summary>
+public partial class FrmGen010ExchangeRates : Form
+{
+    public FrmGen010ExchangeRates()
+    {
+        InitializeComponent();
+    }
+}
