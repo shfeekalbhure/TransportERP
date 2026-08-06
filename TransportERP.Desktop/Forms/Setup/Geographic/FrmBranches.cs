242ac6c725b4bf04c114116ff6ce3f5666da7038
shfeekalbhure <shfeek01@gmail.com>
2026-08-07 00:23:35 +0300

    Implement GEN-012 الفروع WinForms designer layout

---
+++ TransportERP.Desktop/Forms/Setup/Geographic/FrmBranches.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+
+namespace TransportERP.Desktop.Forms.Setup.General;
+
+public partial class FrmBranches : Form
+{
+    public FrmBranches()
+    {
+        InitializeComponent();
+    }
+}
