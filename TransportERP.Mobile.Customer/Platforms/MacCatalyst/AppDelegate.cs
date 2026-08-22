cf31dee90d987eb1906aa1727532f92edb941dd5
shfeekalbhure <shfeek01@gmail.com>
2026-08-22 18:43:58 +0300

    fix(mobile): restore MAUI startup and resources

---
+++ TransportERP.Mobile.Customer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TransportERP.Mobile.Customer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
